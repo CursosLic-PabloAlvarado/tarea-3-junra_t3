698c72ce80adbc15b3bed61ece32e425469e3c23
Hack998 <mendezamuel@gmail.com>
2024-09-19 16:38:43 -0600

    Octave Structure

---
+++ Octave/design_filters.m
@@ -0,0 +1,58 @@
+## Copyright 2024 Juan Pablo Rodríguez Cano, Randall Samuel Mendez Loria
+## Diseño de filtros de la tarea 3.
+## EL5805 Procesamiento digital de señales
+
+## desing_filters : Encargado de diseñar los filtros
+
+pkg load signal;
+
+# dB
+r_bp = 1;
+a_br = 50;
+
+# Freciencias de cortes para los filtros
+f_Fpb = 440;
+f_Fpa = 660;
+f_Fpb_i = 220;
+f_Fpb_s = 1000;
+f_Fsb_i = 220;
+f_Fsb_s = 1000;
+
+# ellip
+# Low Pass Filter
+
+# High Pass Filter
+
+# Band Pass Filter
+
+# Band Reject Reject
+
+
+# butter
+# Low Pass Filter
+
+# High Pass Filter
+
+# Band Pass Filter
+
+# Band Reject Reject
+
+
+# cheby1
+# Low Pass Filter
+
+# High Pass Filter
+
+# Band Pass Filter
+
+# Band Reject Reject
+
+
+# cheby2
+# Low Pass Filter
+
+# High Pass Filter
+
+# Band Pass Filter
+
+# Band Reject Reject
